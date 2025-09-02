4e11e24064b4e6d849e577a5d0439812ebaff907
Devanshu Prakash Patil <devanshupatil602@gmail.com>
2025-09-02 11:28:44 +0530

    Main React component serving as the application root, containing routing logic and primary layout structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import LearnersPage from './components/Leraners/LearnersPage'
+import TeachersPage from './components/Teacher/TeachersPage'
+import ParentsPage from './components/Parents/ParentsPage'
+import { Toaster } from 'sonner';
+
+function App() {
+
+
+  return (
+    <>
+    <Toaster position="top-right" richColors/>
+      <Router>
+        <Routes>
+          {/* Define your routes here */}
+          <Route path="/" element={<Home />} />
+          <Route path="/learners" element={<LearnersPage />} />
+          <Route path="/teachers" element={<TeachersPage />} />
+          <Route path="/parents" element={<ParentsPage />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+ 
+export default App
