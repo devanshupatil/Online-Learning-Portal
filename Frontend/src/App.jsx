9534e73b19163a10fda2debca52df8888a2b54e8
Devanshu Prakash Patil <devanshupatil602@gmail.com>
2025-09-16 21:07:21 +0530

    style: minor layout adjustments in App component
    Updated App.jsx for better routing integration.
    
    Changes:
    - Added conditional rendering for auth routes (4 lines).
    
    Rationale: Improves navigation flow between components.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home'
 import LearnersPage from './components/Leraners/LearnersPage'
 import TeachersPage from './components/Teacher/TeachersPage'
 import ParentsPage from './components/Parents/ParentsPage'
+import AuthPage from './components/Auth/AuthPage'
 import { Toaster } from 'sonner';
 
 function App() {
@@ -17,9 +18,12 @@ function App() {
         <Routes>
           {/* Define your routes here */}
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/signup" element={<AuthPage />} />
           <Route path="/learners" element={<LearnersPage />} />
           <Route path="/teachers" element={<TeachersPage />} />
           <Route path="/parents" element={<ParentsPage />} />
+
         </Routes>
       </Router>
     </>
