acbdc44453798227d4840d21985c2c5907273fbd
Devanshu Prakash Patil <devanshupatil602@gmail.com>
2025-11-11 21:13:45 +0530

    Create the API route to call the getAllTextAnalysis

--- Backend/routes/leraner-routes.js
+++ Backend/routes/leraner-routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getStudyMaterials } = require('../controllers/student-controller');
+const { getStudyMaterials, getAllTextAnalysis } = require('../controllers/student-controller');
 const { getAttendanceByDateAndClass, saveAttendance, getStudentInfo, getStudentAttendanceSummary } = require('../controllers/student-controller');
 
 router.get('/studyMaterials/:learnerId', getStudyMaterials);
+
+router.get('/textAnalysis', getAllTextAnalysis);
 
 // // Routes for student information (for teacher dashboard)
 // router.get('/attendance', getAttendanceByDateAndClass); // Get attendance by date and class
