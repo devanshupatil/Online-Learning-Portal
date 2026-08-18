7448e838c39983a8c63b3e0c4bd326188b08fba4
Devanshu Prakash Patil <devanshupatil692@gmail.com>
2026-08-18 16:53:29 +0530

    add animated theme toggle component and demo

---
+++ Frontend/src/components/ui/animated-theme-toggle-demo.jsx
@@ -0,0 +1,9 @@
+import { AnimatedThemeToggle } from "@/components/ui/animated-theme-toggle";
+
+export default function AnimatedThemeToggleDemo() {
+  return (
+    <>
+      <AnimatedThemeToggle />
+    </>
+  );
+}
